c8b44058347fb5343055fa29a1b5463cb58e6e17
v-j-p <v.poigner@gmail.com>
2024-03-23 15:42:39 +0100

    removes unstable special channel

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -129,24 +129,10 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
     # Configure your nixpkgs instance
     config = {
       allowUnfree = true;
-      packageOverrides = pkgs: {
-        unstable = import <nixpkgs-unstable> {
-          config = config.nixpkgs.config;
-         };
-       };
     };
   };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
