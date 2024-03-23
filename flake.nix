649df187956b9002d1cc4177ed75635ac8f3c7a5
v-j-p <v.poigner@gmail.com>
2024-03-23 18:00:41 +0100

    nixvim added

--- flake.nix
+++ flake.nix
@@ -9,9 +9,12 @@ url = "github:nix-community/home-manager";
 inputs.nixpkgs.follows = "nixpkgs";
 };
 
-# nixvim.url = "github:nix-community/nixvim";
-# flake-parts.url = "github:hercules-ci/flake-parts";
+nixvim = {
+url = "github:nix-community/nixvim";
+inputs.nixpkgs.follows = "nixpkgs";
 };
+flake-parts.url = "github:hercules-ci/flake-parts";
+}
 
 outputs = { self, nixpkgs, ... }@inputs:
 let
@@ -23,7 +26,7 @@ ganesha = nixpkgs.lib.nixosSystem {
 specialArgs = { inherit inputs; };
 modules = [
 ./hosts/ganesha/configuration.nix 
-# ./modules/nixvim/flake.nix
+./modules/nixvim/config/default.nix
 inputs.home-manager.nixosModules.default
 ];
 };
