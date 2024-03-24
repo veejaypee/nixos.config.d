6f14df30e3cdbbc3c31552b3ab9db15f0fde1773
v-j-p <v.poigner@gmail.com>
2024-03-24 17:06:08 +0100

    latest nvim

--- flake.nix
+++ flake.nix
@@ -1,37 +1,43 @@
 {
-description = "Veits base flake";
+  description = "Veits base flake";
 
-inputs = {
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-home-manager = {
-url = "github:nix-community/home-manager";
-inputs.nixpkgs.follows = "nixpkgs";
-};
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-nixvim = {
-url = "github:nix-community/nixvim";
-inputs.nixpkgs.follows = "nixpkgs";
-};
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-flake-parts.url = "github:hercules-ci/flake-parts";
-};
+    nix-colors.url = "github:misterio77/nix-colors";
+  };
 
-outputs = { self, nixpkgs, nixvim, flake-parts, home-manager, ... }@inputs:
-
-    let
-system = "x86_64-linux";
-nixconf = import ./hosts/ganesha/configuration.nix {inherit inputs;};
-      in {
-nixosConfigurations = {
-ganesha = nixpkgs.lib.nixosSystem {
-specialArgs = { inherit inputs; };
-modules = [
-./hosts/ganesha/configuration.nix 
-inputs.home-manager.nixosModules.default
-];
-};
-};
-};
+  outputs = {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-parts,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    nixconf = import ./hosts/ganesha/configuration.nix {inherit inputs;};
+  in {
+    nixosConfigurations = {
+      ganesha = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/ganesha/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+  };
 }
--- hosts/ganesha/home.nix
+++ hosts/ganesha/home.nix
@@ -17,8 +17,86 @@
       flavour = "mocha";
     };
     plugins = {
+      cmp-snippy.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-nvim-lsp-document-symbol.enable = true;
+      cmp-nvim-lsp-signature-help.enable = true;
+      cmp-buffer.enable = true;
+      cmp-cmdline.enable = true;
+      cmp-path.enable = true;
+      cmp-treesitter.enable = true;
+      cmp = {
+        enable = true;
+
+        autoEnableSources = true;
+        settings = {
+          snippet.expand = "snippy";
+          sources = [
+            {name = "snippy";}
+            {name = "nvim_lsp";}
+            {name = "nvim_lsp_document_symbol";}
+            {name = "nvim_lsp_signature_help";}
+            {name = "buffer";}
+            {name = "cmdline";}
+            {name = "path";}
+            {name = "treesitter";}
+          ];
+          mapping = {
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<Tab>" = "cmp.mapping.select_next_item()";
+            "<S-Tab>" = "cmp.mapping.select_prev_item()";
+          };
+        };
+      };
       lsp = {
         enable = true;
+        keymaps = {
+          diagnostic = {
+            "[d" = {
+              action = "goto_next";
+              desc = "Goto next diagnostic";
+            };
+            "]d" = {
+              action = "goto_prev";
+              desc = "Goto previous diagnostic";
+            };
+            "gl" = {
+              action = "open_float";
+              desc = "Open float";
+            };
+          };
+
+          lspBuf = {
+            K = {
+              action = "hover";
+              desc = "Hover";
+            };
+            gr = {
+              action = "references";
+              desc = "Goto references";
+            };
+            gd = {
+              action = "definition";
+              desc = "Goto definition";
+            };
+            gi = {
+              action = "implementation";
+              desc = "Goto implementation";
+            };
+            gt = {
+              action = "type_definition";
+              desc = "Type definition";
+            };
+            gs = {
+              action = "signature_help";
+              desc = "Signature help";
+            };
+            "<F2>" = {
+              action = "rename";
+              desc = "Rename variable";
+            };
+          };
+        };
       };
       none-ls = {
         enable = true;
@@ -30,6 +108,9 @@
       telescope = {
         enable = true;
       };
+
+      oil.enable = true;
+
       # bufferline = {
       #   enable = true;
       # };
@@ -62,6 +143,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    discord
     vesktop
 
     # # Adds the 'hello' command to your environment. It prints a friendly
