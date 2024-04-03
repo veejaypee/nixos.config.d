01cc456aab20cec11836b7271e4f98fda44ad073
v-j-p <v.poigner@gmail.com>
2024-04-03 15:51:02 +0200

    nvidia stuff

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -88,8 +88,8 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = true;
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
