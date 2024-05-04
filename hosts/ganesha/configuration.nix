f8b2d7da53d0eea5335a99cf0840b454b159cd6a
v-j-p <v.poigner@gmail.com>
2024-05-04 09:47:40 +0200

    adds gamemode

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -179,6 +179,8 @@
   # };
   environment.variables.EDITOR = "nvim";
 
+  programs.gamemode.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
