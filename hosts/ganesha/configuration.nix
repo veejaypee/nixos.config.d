c8a844e52dce738c3954fc54c40d668642b1cdf7
v-j-p <v.poigner@gmail.com>
2024-05-05 11:03:47 +0200

    fixes nvidia typo

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -96,7 +96,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackiges.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
