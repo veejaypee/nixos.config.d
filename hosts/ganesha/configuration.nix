3696d26150a0629b7d9f35a6e5434b184eae525b
v-j-p <v.poigner@gmail.com>
2024-06-05 19:00:57 +0200

    uses the 535 nvidia driver

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -46,11 +46,6 @@
 
   # Hardware
   sound.enable = true;
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
 
   security.rtkit.enable = true;
   services.pipewire = {
@@ -81,6 +76,26 @@
   };
   services.xserver.videoDrivers = ["nvidia"];
   # nVidia
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  hardware.nvidia.package = let
+    rcu_patch = pkgs.fetchpatch {
+      url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+      hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+    };
+  in
+    config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "535.154.05";
+      sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+      sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+      openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+      settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+      persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+      patches = [rcu_patch];
+    };
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
@@ -105,7 +120,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
