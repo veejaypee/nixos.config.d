3d456f4e092970a4c821169dd5c7508dc9ad4a48
v-j-p <v.poigner@gmail.com>
2024-10-24 19:05:56 +0200

    update even more

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -34,7 +34,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Hardware
-  sound.enable = true;
+  # sound.enable = true;
 
   security.rtkit.enable = true;
   services.pipewire = {
