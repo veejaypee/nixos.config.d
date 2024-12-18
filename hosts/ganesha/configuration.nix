dd430a459794724baa45feaf6114c9c9c6c8be14
v-j-p <v.poigner@gmail.com>
2024-12-18 10:27:11 +0100

    adds the latest unkown changes

--- hosts/ganesha/configuration.nix
+++ hosts/ganesha/configuration.nix
@@ -21,9 +21,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  security.polkit.enable = true;
-
+  # https://nixos.wiki/wiki/OBS_Studio
   environment.pathsToLink = ["/libexec"]; # links /libexec from derivations to /run/current-system/sw
 
   networking.hostName = "ganesha"; # Define your hostname.
@@ -82,21 +80,21 @@
     enable = true;
     enable32Bit = true;
   };
-  hardware.nvidia.package = let
-    rcu_patch = pkgs.fetchpatch {
-      url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-      hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-    };
-  in
-    config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "535.154.05";
-      sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-      sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-      openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-      settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-      persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-      patches = [rcu_patch];
-    };
+  #  hardware.nvidia.package = let
+  #    rcu_patch = pkgs.fetchpatch {
+  #      url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+  #      hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+  #    };
+  #  in
+  #    config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  #      version = "535.154.05";
+  #      sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+  #      sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+  #      openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+  #      settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+  #      persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+  #      patches = [rcu_patch];
+  #    };
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
@@ -203,6 +201,7 @@
 
     ## Sound
     pavucontrol
+    helvum
 
     # hardware - mouse
     libratbag
@@ -229,10 +228,10 @@
     # python3
 
     node2nix
-    # nodejs
-    # typescript
-    # typescript-language-server
-    # vscode-extensions.angular.ng-template
+    nodejs
+    typescript
+    typescript-language-server
+    vscode-extensions.angular.ng-template
     # ruby
     rustup
 
@@ -276,6 +275,7 @@
     openjdk17
     openjdk8
     prismlauncher
+    cubiomes-viewer
 
     # stream
     (pkgs.wrapOBS {
