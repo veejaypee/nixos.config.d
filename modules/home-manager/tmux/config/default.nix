6d9e0cd2b4317f2c0e0763704d853e37bb616960
v-j-p <v.poigner@gmail.com>
2025-02-22 14:48:25 +0100

    adds tmux styling and flavor

--- modules/home-manager/tmux/config/default.nix
+++ modules/home-manager/tmux/config/default.nix
@@ -11,11 +11,18 @@
     baseIndex = 1;
     keyMode = "vi";
 
+    plugins = with pkgs; [
+      tmuxPlugins.catppuccin
+    ];
+
     extraConfig = ''
       set -g default-terminal "tmux-256color"
-      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides ",xterm-256color:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
+
+      set -g @catppuccin_flavor 'mocha'
+      set -g @catppuccin_window_status_style "rounded"
     '';
   };
 }
