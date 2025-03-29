acffe14b07e1ca0790f9a667a85dd0a0104536b4
v-j-p <v.poigner@gmail.com>
2025-03-29 20:47:33 +0100

    starship config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -10,6 +10,21 @@
   ];
   programs = {
     alacritty.enable = true;
-    starship.enable = true;
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        add_newline = false;
+        directory = {
+          truncation_symbol = ".../";
+          truncate_to_repo = false;
+        };
+        character = {
+          success_symbol = "[>](bright-blue bold)";
+          error_symbol = "[x](bright-red bold)";
+        };
+      };
+    };
   };
 }
