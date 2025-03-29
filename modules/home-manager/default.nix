2c5e2e66041206a4e8c4f6fe1ea5789738e09d4b
v-j-p <v.poigner@gmail.com>
2025-03-29 22:39:44 +0100

    zsh set to emacs keymap

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -8,15 +8,28 @@
   imports = [
     ./tmux.nix
   ];
+  home.shell.enableZshIntegration = true;
   programs = {
     alacritty.enable = true;
 
-    zsh.enable = true;
+    zsh = {
+      enable = true;
+      defaultKeymap = "emacs";
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
+    };
+
     starship = {
       enable = true;
       enableZshIntegration = true;
       settings = {
         add_newline = false;
+        directory = {
+          truncation_symbol = ".../";
+          truncate_to_repo = false;
+        };
         character = {
           success_symbol = "[](bold green) ";
           error_symbol = "[✗](bold red) ";
