288cfa35d6c03189ca31dea1a3209a161672afde
Veit Poigner <veit.poigner@sclable.com>
2025-07-24 12:43:50 +0200

    adds obs

--- hosts/yorishiro/configuration.nix
+++ hosts/yorishiro/configuration.nix
@@ -138,6 +138,19 @@
 
   virtualisation.docker.enable = true;
 
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
