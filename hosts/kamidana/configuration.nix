c9d205853f362f999555697474b244d973a4f3f8
Veit Poigner <v.poigner@gmail.com>
2025-07-30 21:39:52 +0200

    init of kamidana

---
+++ hosts/kamidana/configuration.nix
@@ -0,0 +1,205 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  config,
+  stylix,
+  hyprland,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    ../../modules/google-chrome/config
+    ../../modules/stylix/config
+    ../../modules/hyprland
+    ../../modules/fonts
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "kamidana"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  programs.firefox = {
+    enable = true;
+    nativeMessagingHosts.packages = with pkgs; [
+      tridactyl-native
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Vienna";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
+  # Configure keymap in X11 keeping for XWayland
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "altgr-intl";
+  };
+
+  # Wayland ENVs for chromium based apps
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  ## Sound
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vjp = {
+    isNormalUser = true;
+    description = "Veit Poigner";
+    extraGroups = ["networkmanager" "wheel" "audio" "storage" "input" "docker"];
+    packages = with pkgs; [];
+  };
+
+  environment.shells = with pkgs; [zsh];
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  virtualisation.docker.enable = true;
+
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    wofi
+    networkmanagerapplet
+    docker-compose
+    dunst
+
+    yazi
+    ## Sound Patchbay
+    helvum
+    pavucontrol
+
+    # sys utils
+    unzip
+
+    ripgrep
+    fzf #fuzzySearchFinder
+    fd
+    neofetch
+
+    glib # gsettings
+
+    spotify
+
+    ## Langs
+    gcc
+    #python3
+    nodePackages_latest.nodejs
+    nodenv
+
+    ##Language Server - LS
+    angular-language-server
+    nodePackages_latest.jsonlint
+    vscode-langservers-extracted
+
+    node2nix
+
+    # tools
+    wl-clipboard
+  ];
+
+  ## Login Manager
+  services = {
+    displayManager.sddm = {
+      enable = true;
+      autoNumlock = true;
+      wayland.enable = true;
+    };
+  };
+
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = {inherit inputs;};
+    sharedModules = [
+      ../../modules/stylix/config
+    ];
+    users.vjp = {
+      imports = [
+        ./home.nix
+      ];
+    };
+  };
+  environment.variables.EDITOR = "nvim";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
