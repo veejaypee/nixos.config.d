f9856ff3299acd995cb852398460b8b0ed77f492
Veit Poigner <v.poigner@gmail.com>
2025-08-29 12:26:32 +0200

    add tmux nvim navigator

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -7,18 +7,43 @@
 }: {
   programs.tmux = {
     enable = true;
+    shortcut = "Space";
     clock24 = true;
     baseIndex = 1;
     keyMode = "vi";
+    mouse = true;
 
     plugins = with pkgs; [
       tmuxPlugins.catppuccin
+      tmuxPlugins.continuum
+      tmuxPlugins.resurrect
+      tmuxPlugins.vim-tmux-navigator
     ];
 
     extraConfig = ''
       set -g default-terminal "tmux-256color"
       set -ag terminal-overrides ",xterm-256color:Tc"
+
       set -g @catppuccin_flavor 'mocha'
+
+      set -g escape-time 0
+      set -g status-interval 3
+      set -g set-clipboard on
+      set -g detach-on-destroy off
+      set -g history-limit 1000000
+      set -g allow-passthrough on
+
+      # Split horizontally in CWD with \
+      unbind %
+      bind \\ split-window -h -c "#{pane_current_path}"
+
+      # Split vertically in CWD with -
+      unbind \"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      set -g renumber-windows on # Automatically renumber windows when one is closed
+
+      set -g @resurrect-capture-pane-contents 'on'
     '';
   };
 }
