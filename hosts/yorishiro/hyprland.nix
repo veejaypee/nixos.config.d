9e25bf0c56b14c0956920024511f0578244fe733
Veit Poigner <veit.poigner@sclable.com>
2025-09-22 12:53:38 +0200

    adds wallpaper and possibly opacity

--- hosts/yorishiro/hyprland.nix
+++ hosts/yorishiro/hyprland.nix
@@ -334,5 +334,7 @@
 
     # Fix some dragging issues with XWayland
     windowrule = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
+
+    windowrule = opacity 0.95,class:(*)
   '';
 }
