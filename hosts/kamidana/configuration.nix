3c3b8e5f00eea718f3641929b8c814a49f100c1d
Veit Poigner <v.poigner@gmail.com>
2025-11-11 20:36:43 +0100

    chore: gamescope

--- hosts/kamidana/configuration.nix
+++ hosts/kamidana/configuration.nix
@@ -201,6 +201,7 @@
     gamescopeSession.enable = true;
   };
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
